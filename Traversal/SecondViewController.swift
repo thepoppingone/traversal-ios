e4b505a78ff5b584bb5940c12bda07a38c113131
Poh Peng <wangpp1@hotmail.com>
2016-04-19 00:52:12 -0700

    Initial Commit

---
+++ Traversal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Traversal
+//
+//  Created by POH PENG WANG on 4/19/16.
+//  Copyright © 2016 Traversal Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
